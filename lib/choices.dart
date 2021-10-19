481f529f192d8cf7b4fff8fb0bb99f991e7b49c6
fhkhd <fhkhdkri.yahoo.com>
2021-10-19 06:01:18 +0330

    deletlist

--- lib/choices.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'MainHome.dart';
-class ChoiceOfTransport {
-  ChoiceOfTransport({this.title, this.item});
-  String title;
-  // InkWell widget;
-  var item;
-  // InkWell inkWell;
-}
